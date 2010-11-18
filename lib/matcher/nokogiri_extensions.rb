1b1ce417334254c999f26ff4f62bc60cd04142e9
Peter Moran <workingpeter@gmail.com>
2010-11-19 06:59:12 +1100

    Nokogiri extensions simplified

--- lib/matcher/nokogiri_extensions.rb
+++ lib/matcher/nokogiri_extensions.rb
@@ -4,15 +4,29 @@ module Nokogiri
 
   module XML
 
+    class Node
+
+      def matching(other, matcher)
+        other_elem = other.at_xpath(path)
+        matcher.record(self, false, "not found") unless other_elem
+        other_elem
+      end
+    end
+
+    class Document
+
+      def match?(other, matcher)
+        matching(other, matcher)
+      end
+
+    end
+
     class Element
 
       def match?(other, matcher)
         @matcher = matcher
-        other_elem = other.at_xpath(path)
-        unless other_elem
-          @matcher.record(self, false, "not found")
-          return false
-        end
+        other_elem = matching(other, matcher)
+        return false unless other_elem
         children_match?(other_elem) & attributes_match?(other_elem)
       end
 
@@ -26,57 +40,44 @@ module Nokogiri
 
         def attributes_match?(other)
           match = attributes.size == other.attributes.size
-          if match
-            attributes.values.each do |attr|
-              match = match & attr.match?(other, @matcher)
-            end
-          else
+          unless match
             @matcher.record(self, match, "expected #{attributes.size} attributes, got #{other.attributes.size}")
+            return false
           end
+
+          attributes.values.each { |attr|  match = match & attr.match?(other, @matcher) }
           match
         end
 
     end
 
-    class Document
-      
-      def match?(other, matcher = nil)
-        true
-      end
-      
-    end
-
     class Text
-      
+
       def match?(other, matcher)
         @matcher = matcher
-        other_elem = other.at_xpath(path)
-        unless other_elem
-          @matcher.record(self, false, "not found")
-          return false
-        end
+        other_elem = matching(other, matcher)
+        return false unless other_elem
 
         custom_matcher = matcher.custom_matchers[path]
         match = custom_matcher ? custom_matcher.call(other_elem) : (content == other_elem.content)
         @matcher.record(self, match, "expected '#{content}', got '#{other_elem.content}'")
         match
       end
-      
+
     end
 
     class Attr
+
       def match?(other, matcher)
-        other_elem = other.at_xpath(path)
-        unless other_elem
-          matcher.record(self, false, "not found")
-          return false
-        end
-        
+        other_elem = matching(other, matcher)
+        return false unless other_elem
+
         custom_matcher = matcher.custom_matchers[path]
         match = custom_matcher ? custom_matcher.call(other_elem) : (value == other_elem.value)
         matcher.record(self, match, "expected '#{value}', got '#{other_elem.value}'")
         match
       end
+
     end
 
   end
